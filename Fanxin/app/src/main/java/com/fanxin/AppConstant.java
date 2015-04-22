bd9cb295396fa05fc4558ed247397856ba2e8ab1
unknown <tiazh@longnote.fareast.corp.microsoft.com>
2015-04-22 23:18:01 +0800

    check is login

---
+++ Fanxin/app/src/main/java/com/fanxin/AppConstant.java
@@ -0,0 +1,8 @@
+package com.fanxin;
+
+/**
+ * Created by tiazh on 4/22/2015.
+ */
+public class AppConstant {
+    public static boolean isLogin = true;
+}
--- Fanxin/app/src/main/java/common/Constant.java
+++
@@ -1,7 +0,0 @@
-package common;
-
-/**
- * Created by tiazh on 4/21/2015.
- */
-public class Constant {
-}
